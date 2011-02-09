e702649f82a48b27181598932983ecb466288367
Yucheng Low <Yucheng Low@localhost>
2011-02-09 02:43:38 -0500

    rpcexec.py now has muchbetter screen support

---
+++ src/graphlab/rpc/reply_increment_counter.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <graphlab/rpc/dc.hpp>
+#include <graphlab/rpc/reply_increment_counter.hpp>
+
+namespace graphlab {
+
+void reply_increment_counter(distributed_control &dc, procid_t src, 
+                             size_t ptr, dc_impl::blob ret) {
+  dc_impl::reply_ret_type *a = reinterpret_cast<dc_impl::reply_ret_type*>(ptr);
+  a->val=ret;
+  a->flag.inc();  
+  if (a->usesem) a->sem.post();
+}
+
+}
