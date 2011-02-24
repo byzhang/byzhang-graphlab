97858e95e936d6aff6f9044c5192e146c8368a60
Danny Bickson <danny.bickson@gmail.com>
2011-02-24 10:33:07 -0500

    added a clean version of gabp cleaned by the great joseph gonzalez!

---
+++ src/graphlab/rpc/reply_increment_counter.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <graphlab/rpc/dc.hpp>
+#include <graphlab/rpc/reply_increment_counter.hpp>
+
+namespace graphlab {
+
+void reply_increment_counter(distributed_control &dc, procid_t src, 
+                             size_t ptr, dc_impl::blob ret) {
+  dc_impl::reply_ret_type *a = reinterpret_cast<dc_impl::reply_ret_type*>(ptr);
+  a->mut.lock();
+  a->val=ret;
+  size_t retval = a->flag.dec();  
+  if (retval == 0 && a->usemutex) {
+    a->cond.signal();
+  }
+  a->mut.unlock();
+}
+
+}
