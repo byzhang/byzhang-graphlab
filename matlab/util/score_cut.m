364999b61b5de1264188dd12c0285cd99e124260
Joseph E. Gonzalez <joseph.e.gonzalez@gmail.com>
2011-03-05 18:52:49 -0500

    uint32_t to vertex_id_t

---
+++ matlab/util/score_cut.m
@@ -0,0 +1,27 @@
+function score = score_cut(cut, wE, wV) 
+[n,n] = size(wE);
+[u,v,w] = find(wE);
+
+p = length(unique(cut));
+
+vCut = full(sum(...
+   sparse(u,v, w .* (cut(u) ~= cut(v)), n,n) ...
+   ))';
+
+% For each processor compute the work and communication done by tha
+% processor
+parfor i = unique(cut(:))';
+   score.work(i) = sum( wV(cut == i) );
+   score.ework(i) = sum( sum(wE(cut == i,:)) );
+   score.comm(i) = sum( vCut(cut == i) );
+end
+
+
+score.cost = sum(vCut(:)); %sum(score.comm);
+score.bal = max(score.work) * p / sum(wV);
+
+
+score.ebal = max(score.ework) * p / sum(wE(:));
+
+% score.value = max(score.work) + sum(vCut);
+end
