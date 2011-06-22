80f02574c1ea76c2b6ada3a4196f7b0e947d3c11
Joseph E. Gonzalez <joseph.e.gonzalez@gmail.com>
2011-06-22 12:27:46 -0700

    I reorganized the scripts in the scripts folder and removed a lot of the hard coded behaviour in configure allowing config.deps to customize the installation.  I also incrementally construct configure.deps so if configure partially succeeds (e.g., installs cmake bu fails to install KC) then the cmake configuration is saved. Finally, and most importantly, I added system information collection and logging (to config.log) which should collect most of the information describing the configure process.
    Unfortunately I am only able to test on my mac/linux box and will need
    further assistance from the team.

---
+++ src/graphlab/rpc/mem_function_arg_types_undef.hpp
@@ -0,0 +1,38 @@
+/*
+This file is part of GraphLab.
+
+GraphLab is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as 
+published by the Free Software Foundation, either version 3 of 
+the License, or (at your option) any later version.
+
+GraphLab is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public 
+License along with GraphLab.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#undef __GLRPC_NIF0
+#undef __GLRPC_NIF1
+#undef __GLRPC_NIF2
+#undef __GLRPC_NIF3
+#undef __GLRPC_NIF4
+#undef __GLRPC_NIF5
+#undef __GLRPC_NIF6
+#undef __GLRPC_NIF7
+#undef __GLRPC_R0
+#undef __GLRPC_R1
+#undef __GLRPC_R2
+#undef __GLRPC_R3
+#undef __GLRPC_R4
+#undef __GLRPC_R5
+#undef __GLRPC_R6
+#undef __GLRPC_R7
+
+#undef __GLRPC_FRESULT
+#undef REMOVE_CONST_REF
+#undef __GLRPC_FARITY
+
---
+++ src/graphlab/util/util_includes.hpp
@@ -0,0 +1,27 @@
+/*
+This file is part of GraphLab.
+
+GraphLab is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as 
+published by the Free Software Foundation, either version 3 of 
+the License, or (at your option) any later version.
+
+GraphLab is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public 
+License along with GraphLab.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include <graphlab/util/binary_parser.hpp>
+#include <graphlab/util/command_line_options.hpp>
+#include <graphlab/util/timer.hpp>
+#include <graphlab/util/generics/blob.hpp>
+#include <graphlab/util/random.hpp>
+#include <graphlab/util/small_set.hpp>
+#include <graphlab/util/charstream.hpp>
+
+
