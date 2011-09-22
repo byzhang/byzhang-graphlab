2256bae60bc2b33f6a64e9f857f32ff408a37911
Yucheng Low <Yucheng Low@localhost>
2011-09-22 00:39:27 -0400

    Debugging MR construction to memory

---
+++ demoapps/pmf/tensor.hpp
@@ -0,0 +1,183 @@
+#ifndef _TENSOR_HPP
+#define _TENSOR_HPP
+
+extern advanced_config ac;
+extern problem_setup ps;
+
+#include <graphlab/macros_def.hpp>
+std::vector<edge_id_t> * edges;  //edge list for pointing from time nodes into their connected users/movies (for tensor)
+double muT = 1; //mean of time nodes
+
+// Calculate time nodes (for tensor)
+void calc_T(int i){
+
+  assert(ps.tensor);
+ 
+  assert(i >=0 && i < ps.K);
+  if (ac.zero && edges[i].size() == 0){
+     if (i == ps.K-1)
+        last_iter();
+    return;
+  }
+
+  assert(edges[i].size() > 0);
+
+  if (ac.debug && (i==0 || i == ps.K-1))
+    printf("node %d with Q size: %d %d\n", i, (int)edges[i].size(), (int)ac.D);
+
+  int k=0;
+  int batchSize = 100;
+  mat Q(batchSize, ac.D); 
+  vec vals(batchSize);
+  mat QQ = zeros(ac.D,ac.D);
+  vec RQ = zeros(ac.D);
+  int cnt =0;
+
+  timer t; t.start();
+  foreach (edge_id_t edge, edges[i]){
+    if (k % batchSize == 0){
+      Q = zeros(batchSize, ac.D);
+      vals = zeros(batchSize);
+      cnt = 1;
+    }
+
+    //find the right edge which matches the current time
+#ifndef GL_NO_MULT_EDGES    
+     multiple_edges * medges= &ps.g->edge_data(edge);
+      edge_data data;
+      for (int j=0; j< (int)medges->medges.size(); j++){
+        data = medges->medges[j];
+        if (data.time == i)
+          break;
+      }
+#else
+     edge_data data= ps.g->edge_data(edge);
+#endif
+    assert(data.time == i);
+
+    assert((int)ps.g->target(edge)>= ps.M);
+    assert((int)ps.g->source(edge)< ps.M);
+    vertex_data * v1 = &ps.g->vertex_data(ps.g->target(edge));
+    vertex_data * v2 = &ps.g->vertex_data(ps.g->source(edge));
+    vec ret = elem_mult(v1->pvec, v2->pvec);
+     
+    for (int s=0; s<ac.D; s++)
+      Q(k%batchSize,s)=ret(s);
+    if (ac.debug && (i==0 || i == ps.K-1) && (k == 0 || k == (int)edges[i].size() - 1))
+      std::cout<<" clmn "<<k<< " vec: " << ret<<std::endl;
+
+    vals[k%batchSize] = data.weight;
+    k++;
+
+    if ((cnt  == batchSize) || (cnt < batchSize && k == (int)edges[i].size()-1)){
+      QQ += transpose(Q)*Q;
+      RQ += transpose(Q)*vals;
+      assert(QQ.rows() == ac.D && QQ.cols() == ac.D);
+    }
+    cnt++;
+  }
+  ps.counter[BPTF_TIME_EDGES] += t.current_time();
+
+
+  if (ac.debug && (i == 0 ||i== ps.K-1 )){
+    std::cout<<"QQ:"<<QQ<<std::endl;
+    std::cout<<"RQ:"<<RQ<<std::endl;
+  }
+
+  assert(RQ.size() == ac.D);
+  assert((unsigned int)k == edges[i].size());
+  vec sol;    
+  vec out;
+
+  t.start();
+  if (i == 0){
+    vec t1 = ps.times[1].pvec; 
+    //calc least squares estimation of time nodes
+    if (!ps.BPTF){
+      QQ = QQ+ 2*eDT;
+      bool ret = ls_solve(QQ, ps.pT*(t1 + vones*muT)+ RQ, out);
+      assert(ret);
+    }
+    //calc equation A.8 in Xiong paper
+    else {
+      mat A_k = 2*A_T+alpha*QQ;
+      mat iAk_;
+      bool ret = inv(A_k, iAk_);
+      assert(ret);
+      vec muk_ = iAk_*(A_T*(t1 + mu_T)+alpha*RQ); //TODO
+      out = mvnrndex(muk_, iAk_, ac.D);
+    }
+  }
+  else if (i == ps.K-1){
+    vec tk_2 = ps.times[ps.K-2].pvec; 
+    //calc least squares estimation of time nodes
+    if (!ps.BPTF){
+      QQ = QQ + eDT;
+      bool ret = ls_solve(QQ, ps.pT*tk_2 + RQ, out);
+      assert(ret); 
+    }
+    //calc equation A.8 in Xiong paper
+    else {
+      mat A_k = A_T+alpha*QQ;
+      mat iAk_; 
+      bool ret = inv(A_k, iAk_);
+      assert(ret);
+      vec muk_ = iAk_*(A_T*tk_2+alpha*RQ); //TODO
+      out = mvnrndex(muk_, iAk_, ac.D);
+    }
+  }
+  else {
+    vec tsum; 
+    vec t1 = ps.times[i-1].pvec; 
+    vec t2 = ps.times[i+1].pvec;
+    tsum = t1+t2;
+    //calc least squares estimation of time nodes
+    if (!ps.BPTF){
+      QQ = QQ + 2*eDT;
+      bool ret = ls_solve(QQ, ps.pT*tsum + RQ, out);
+      assert(ret);
+    }
+    //calc equation A.8 in Xiong paper
+    else {
+      mat A_k = 2*A_T+alpha*QQ;
+      mat iAk_;
+      bool ret = inv(A_k, iAk_);
+      assert(ret);
+      vec muk_ = iAk_*(A_T* tsum +alpha*RQ); //TODO
+      out = mvnrndex(muk_, iAk_, ac.D);
+    }
+  }
+
+  ps.times[i].pvec = out;
+  if (ac.debug && (i == 0|| i == ps.K-1)) 
+    std::cout<<ps.times[i].pvec<<std::endl;
+  assert(QQ.rows() == ac.D && QQ.cols() == ac.D);
+  ps.counter[BPTF_LEAST_SQUARES] += t.current_time();
+  //}
+
+  if (i == ps.K-1){
+    last_iter();
+  }
+          
+
+
+}
+// update function for time nodes
+// this function is called only in tensor mode
+void time_node_update_function(gl_types::iscope &scope, gl_types::icallback &scheduler) {
+
+  assert(ps.tensor);
+
+  int id = scope.vertex() - ps.M-ps.N;
+  assert(id >=0 && id < ps.K);	
+  if (ac.debug && (id == 0 || id == ps.K-1)){
+    printf("entering time node  %d \n", id);   
+  } 
+  if (ps.K > 1)
+    calc_T(id); 
+  else last_iter();
+}
+
+
+#include <graphlab/macros_undef.hpp>
+#endif //_TENSOR_HPP
---
+++ src/graphlab/graph/memory_atom.cpp
@@ -0,0 +1,370 @@
+#include <sstream>
+#include <map>
+#include <graphlab/serialization/serialization_includes.hpp>
+#include <graphlab/graph/memory_atom.hpp>
+#include <graphlab/logger/logger.hpp>
+
+namespace graphlab {
+
+memory_atom::memory_atom(std::string filename, uint16_t atomid):atomid(atomid),filename(filename) {
+  std::ifstream fin(filename.c_str(), std::ios::binary);
+  if (fin.good() && fin.is_open()) {
+    iarchive iarc(fin);
+    uint64_t nv,ne;
+    iarc >> nv >> ne
+         >> vertices >> vidmap >> vid2owner_segment;
+    numv.value = nv;
+    nume.value = ne;
+    mutated = false;
+  }
+  else {
+    mutated = true;
+  }
+}
+
+
+void memory_atom::add_vertex(vertex_id_type vid, uint16_t owner) {
+  if (!add_vertex_skip(vid, owner)) {
+    mut.lock();
+    boost::unordered_map<vertex_id_t, size_t>::const_iterator iter = vidmap.find(vid);
+    vertices[iter->second].owner = owner;
+    mutated = true;
+    mut.unlock();
+  }
+}
+
+/**
+ * \brief Inserts vertex 'vid' into the file without data.
+ * If the vertex already exists, nothing will be done.
+ * Returns true if vertex was added.
+ */
+bool memory_atom::add_vertex_skip(vertex_id_type vid, uint16_t owner) {
+  mutated = true;
+  bool ret = false;
+  mut.lock();
+  boost::unordered_map<vertex_id_t, size_t>::const_iterator iter = vidmap.find(vid);
+  if (iter == vidmap.end()) {
+    if (owner == atom_id()) numv.inc();
+    vertices.push_back(vertex_entry(vid, owner));
+    vidmap[vid] = vertices.size() - 1;
+    ret = true;
+  }
+  mut.unlock();
+  return ret;
+}
+
+
+void memory_atom::add_vertex_with_data(vertex_id_type vid, uint16_t owner, const std::string &vdata) {
+  mutated = true;
+  mut.lock();
+  boost::unordered_map<vertex_id_t, size_t>::const_iterator iter = vidmap.find(vid);
+  if (iter == vidmap.end()) {
+    if (owner == atom_id()) numv.inc();
+    vertices.push_back(vertex_entry(vid, owner, -1, vdata));
+    vidmap[vid] = vertices.size() - 1;
+  }
+  else {
+    vertices[iter->second].owner = owner;
+    vertices[iter->second].vdata = vdata;
+  }
+  mut.unlock();
+}
+
+
+void memory_atom::add_edge_with_data(vertex_id_type src, vertex_id_type target, const std::string &edata) {
+  mutated = true;
+  mut.lock();
+  boost::unordered_map<vertex_id_t, size_t>::const_iterator srciter = vidmap.find(src);
+
+  if (srciter == vidmap.end()) {
+    mut.unlock();
+    if (edata.size() > 0) add_vertex_skip(src, uint16_t(-1));     // source may or may not be a ghost
+    else add_vertex_skip(src, atom_id()); // source is definitely local
+    mut.lock();
+    srciter = vidmap.find(src);
+  }
+  size_t vsrc = srciter->second;
+  
+  boost::unordered_map<vertex_id_t, size_t>::const_iterator destiter = vidmap.find(target);
+  if (destiter == vidmap.end()) {
+    mut.unlock();
+    if (edata.size() > 0) add_vertex_skip(target, atom_id());     // target is definitely local
+    else add_vertex_skip(target, uint16_t(-1)); // target is definitely a ghost
+    mut.lock();
+    destiter = vidmap.find(target);
+  }
+  size_t vtarget = destiter->second;
+  mut.unlock();
+  
+  edgemut[vsrc % 511].lock();
+  if (vertices[vsrc].outedges.count(target) == 0) {
+    if (edata.length() > 0) nume.inc();
+    vertices[vsrc].outedges.insert(target);
+  }
+  edgemut[vsrc % 511].unlock();
+  
+  edgemut[vtarget % 511].lock();
+  vertices[vtarget].inedges.insert(std::make_pair(src, edata));
+  edgemut[vtarget % 511].unlock();
+}
+
+void memory_atom::add_edge_with_data(vertex_id_type src, uint16_t srcowner,
+                                     vertex_id_type target, uint16_t targetowner, 
+                                     const std::string &edata) {
+  mutated = true;
+  mut.lock();
+  boost::unordered_map<vertex_id_t, size_t>::const_iterator srciter = vidmap.find(src);
+
+  if (srciter == vidmap.end()) {
+    mut.unlock();
+    add_vertex_skip(src, srcowner);
+    mut.lock();
+    srciter = vidmap.find(src);
+  }
+  size_t vsrc = srciter->second;
+  
+  boost::unordered_map<vertex_id_t, size_t>::const_iterator destiter = vidmap.find(target);
+  if (destiter == vidmap.end()) {
+    mut.unlock();
+    add_vertex_skip(target, targetowner);
+    mut.lock();
+    destiter = vidmap.find(target);
+  }
+  size_t vtarget = destiter->second;
+  mut.unlock();
+  
+  edgemut[vsrc % 511].lock();
+  if (vertices[vsrc].outedges.count(target) == 0) {
+    if (edata.length() > 0) nume.inc();
+    vertices[vsrc].outedges.insert(target);
+  }
+  edgemut[vsrc % 511].unlock();
+  
+  edgemut[vtarget % 511].lock();
+  vertices[vtarget].inedges.insert(std::make_pair(src, edata));
+  edgemut[vtarget % 511].unlock();
+}
+
+
+
+void memory_atom::set_vertex(vertex_id_type vid, uint16_t owner) {
+  mutated = true;
+  mut.lock();
+  boost::unordered_map<vertex_id_t, size_t>::const_iterator iter = vidmap.find(vid);
+  ASSERT_TRUE(iter != vidmap.end());
+  size_t v = iter->second;
+  mut.unlock();
+  vertices[v].owner = owner;
+}
+
+
+void memory_atom::set_vertex_with_data(vertex_id_type vid, uint16_t owner, const std::string &vdata) {
+  mutated = true;
+  mut.lock();
+  boost::unordered_map<vertex_id_t, size_t>::const_iterator iter = vidmap.find(vid);
+  ASSERT_TRUE(iter != vidmap.end());
+  size_t v = iter->second;
+  mut.unlock();
+  vertices[v].owner = owner;
+  vertices[v].vdata = vdata;
+}
+
+
+
+
+void memory_atom::set_edge_with_data(vertex_id_type src, vertex_id_type target, const std::string &edata) {
+  mutated = true;
+  mut.lock();
+  boost::unordered_map<vertex_id_t, size_t>::const_iterator srciter = vidmap.find(src);
+  ASSERT_TRUE(srciter != vidmap.end());
+  size_t vsrc = srciter->second;
+  
+  boost::unordered_map<vertex_id_t, size_t>::const_iterator destiter = vidmap.find(target);
+  ASSERT_TRUE(destiter != vidmap.end());
+  size_t vtarget = destiter->second;
+  mut.unlock();
+  
+  edgemut[vsrc % 511].lock();
+  ASSERT_EQ(vertices[vsrc].outedges.count(target), 1);
+  edgemut[vsrc % 511].unlock();
+  
+  edgemut[vtarget % 511].lock();
+  vertices[vtarget].inedges[src] = edata;
+  edgemut[vtarget % 511].unlock();
+}
+
+
+
+
+
+bool memory_atom::get_vertex(vertex_id_type vid, uint16_t &owner) {
+  mut.lock();
+  boost::unordered_map<vertex_id_t, size_t>::const_iterator iter = vidmap.find(vid);
+  if (iter == vidmap.end()) {
+    mut.unlock();
+    return false;
+  }
+  size_t v = iter->second;
+  mut.unlock();
+  owner = vertices[v].owner;
+  return true;
+}
+
+
+
+bool memory_atom::get_vertex_data(vertex_id_type vid, uint16_t &owner, std::string &vdata) {
+  mut.lock();
+  boost::unordered_map<vertex_id_t, size_t>::const_iterator iter = vidmap.find(vid);
+  
+  if(iter == vidmap.end()) {
+    mut.unlock();
+    return false;
+  }
+  size_t v = iter->second;
+  mut.unlock();
+  
+  owner = vertices[v].owner;
+  vdata = vertices[v].vdata;
+  return true;
+}
+
+/**
+ * \brief Reads a edge from the file returning results in 'owner' and 'vdata'.
+ * Returns true if edge exists and false otherwise.
+ * If there is no edge data stored, edata will not be modified.
+ */
+bool memory_atom::get_edge_data(vertex_id_type src, vertex_id_type target, std::string &edata) {
+  mut.lock();
+  boost::unordered_map<vertex_id_t, size_t>::const_iterator targetiter = vidmap.find(target);
+  if(targetiter == vidmap.end()) {
+    mut.unlock();
+    return false;
+  }
+  size_t vtarget = targetiter->second;
+  mut.unlock();
+  std::map<vertex_id_type, std::string>::const_iterator iter = vertices[vtarget].inedges.find(src);
+  if (iter == vertices[vtarget].inedges.end()) return false;
+  edata = iter->second;
+  return true;
+}
+
+
+std::vector<memory_atom::vertex_id_type> memory_atom::enumerate_vertices() {
+  std::vector<vertex_id_type> ret;
+  ret.resize(vertices.size());
+  for (size_t i = 0; i < ret.size(); ++i) {
+    ret[i] = vertices[i].vid;
+  }
+  return ret;
+}
+
+
+std::map<uint16_t, uint32_t> memory_atom::enumerate_adjacent_atoms() {
+  std::map<uint16_t, uint32_t> ret;
+  for (size_t i = 0; i < vertices.size(); ++i) {
+    if (vertices[i].owner != atomid) {
+      ++ret[vertices[i].owner];
+    }
+  }
+  return ret;
+}
+
+std::vector<memory_atom::vertex_id_type> memory_atom::get_out_vertices(vertex_id_type vid) {
+  std::vector<vertex_id_type> ret;
+  boost::unordered_map<vertex_id_t, size_t>::const_iterator vidmap_iter = vidmap.find(vid);
+  if (vidmap_iter == vidmap.end()) return ret;
+  size_t v = vidmap_iter->second;
+  
+  std::copy(vertices[v].outedges.begin(), 
+            vertices[v].outedges.end(), 
+            std::inserter(ret, ret.begin()));
+  return ret;
+}
+
+
+std::vector<memory_atom::vertex_id_type> memory_atom::get_in_vertices(vertex_id_type vid) {
+  std::vector<vertex_id_type> ret;
+  boost::unordered_map<vertex_id_t, size_t>::const_iterator vidmap_iter = vidmap.find(vid);
+  if (vidmap_iter == vidmap.end()) return ret;
+  size_t v = vidmap_iter->second;
+  
+  std::map<vertex_id_type, std::string>::const_iterator iter = vertices[v].inedges.begin();
+  ret.reserve(vertices[v].inedges.size());
+  
+  while(iter != vertices[v].inedges.end()) {
+    ret.push_back(iter->first);
+    ++iter;
+  }
+  return ret;
+}
+
+
+memory_atom::vertex_color_type memory_atom::get_color(vertex_id_type vid) {
+  mut.lock();
+  vertex_color_type ret = vertex_color_type(-1);
+  boost::unordered_map<vertex_id_t, size_t>::const_iterator viter = vidmap.find(vid);
+  if (viter != vidmap.end()) {
+    ret = vertices[viter->second].color;
+  }
+  mut.unlock();
+  return ret;
+}
+
+
+void memory_atom::set_color(vertex_id_type vid, vertex_color_type color) {
+  mutated = true;
+  mut.lock();
+  std::vector<vertex_id_type> ret;
+  boost::unordered_map<vertex_id_t, size_t>::const_iterator viter = vidmap.find(vid);
+  if (viter != vidmap.end()) {
+    vertices[viter->second].color = color;
+  }
+  mut.unlock();
+
+}
+
+vertex_color_type memory_atom::max_color() {
+  vertex_color_type m = 0;
+  for (size_t i = 0;i < vertices.size(); ++i) {
+    if (vertices[i].color > m) m = vertices[i].color;
+  }
+  return m;
+}
+
+uint16_t memory_atom::get_owner(vertex_id_type vid) {
+  maplock.lock();
+  boost::unordered_map<vertex_id_t, uint16_t>::const_iterator iter = vid2owner_segment.find(vid);
+  uint16_t ret = 0;
+  if (iter != vid2owner_segment.end()) ret = iter->second;
+  else ret = uint16_t(-1);
+  maplock.unlock();
+  return ret;
+}
+
+void memory_atom::set_owner(vertex_id_type vid, uint16_t owner) {
+  mutated = true;
+  maplock.lock();
+  vid2owner_segment[vid] = owner;
+  maplock.unlock();
+}
+
+void memory_atom::clear() {
+  vid2owner_segment.clear();
+  vertices.clear();
+  vidmap.clear();
+}
+
+void memory_atom::synchronize() {
+  if (mutated) {
+    std::ofstream fout(filename.c_str(), std::ios::binary);
+    oarchive oarc(fout);
+    uint64_t nv,ne;
+    nv = numv.value;
+    ne = nume.value;
+  
+    oarc << nv << ne
+         << vertices << vidmap << vid2owner_segment;
+  }
+}
+    
+}
