dfbbbfdd9acae3eb73cb67d61b1eab792bec71b7
jegonzal <jegonzal@localhost>
2011-10-17 04:34:09 +0000

    reduced timed wait

---
+++ demoapps/clustering/distance.h
@@ -0,0 +1,24 @@
+#ifndef _DISTANCE_H
+#define _DISTANCE_H
+
+#include "graphlab.hpp"
+#include "../pmf/mathlayer.hpp"
+
+
+enum distance_measure{
+   EUCLIDEAN = 0,
+   CHEBYCHEV = 1,
+   MANAHOLIS = 2,
+   MANHATTAN = 3,
+   MINKOWSKI = 4,
+   TONIMOTO = 5,
+   WEIGHTED = 6,
+   WEIGHTED_MANAHOLIS = 7,
+   COSINE = 8
+};
+
+
+double calc_distance(sparse_vec & datapoint, vec &cluster, double sqr_sum = 0);
+
+
+#endif //_DISTANCE_H
