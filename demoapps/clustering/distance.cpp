9bd42148b82f8d92af2c147ee64640a8884f92e7
gu haijie <gu.haijie@gmail.com>
2011-10-24 00:33:44 -0400

    In asynchronous engine : separate the updating and syncing functionality into two groups of threads; Disable the async_simulate interface.

---
+++ demoapps/clustering/distance.cpp
@@ -0,0 +1,169 @@
+#include "distance.h"
+#include "clustering.h"
+#include "../gabp/advanced_config.h"
+
+extern advanced_config ac;
+const char * distance_measure_name[] = {"EUCLIDEAN", "CHEBYCHEV", "MANAHOLIS", "MANHATTAN", "MINKOWSKI", "TONIMOTO", "WEIGTED", "WEIGHTED_MANAHOLIS", "COSINE"};
+
+sparse_flt_dbl_vec minus(sparse_flt_dbl_vec & dvec1, sparse_flt_dbl_vec & dvec2);
+vec minus(sparse_flt_dbl_vec & dvec1, vec & dvec2);
+flt_dbl sum_sqr(sparse_flt_dbl_vec & dvec1);
+sparse_flt_dbl_vec fabs(sparse_flt_dbl_vec& dvec1);
+flt_dbl sum(sparse_flt_dbl_vec & dvec);
+
+flt_dbl calc_euclidian_distance( sparse_flt_dbl_vec & datapoint,  sparse_flt_dbl_vec &cluster, flt_dbl sqr_sum, flt_dbl sqr_sum0){
+  //sparse_flt_dbl_vec diff = minus(datapoint , cluster);
+  //return sqrt(sum_sqr(diff));
+  sparse_flt_dbl_vec mult = elem_mult(datapoint, cluster);
+  flt_dbl diff = (sqr_sum + sqr_sum0 - 2*sum(mult));
+  return sqrt(fabs(diff)); //because of numerical errors, diff may be negative
+}
+
+
+flt_dbl calc_euclidian_distance( sparse_flt_dbl_vec & datapoint,  flt_dbl_vec &cluster, flt_dbl sqr_sum, flt_dbl sqr_sum0){
+  flt_dbl dist = sqr_sum + sqr_sum0;
+  //for (int i=0; i< datapoint.nnz(); i++){
+  FOR_ITERATOR(i, datapoint){
+      flt_dbl val = get_nz_data(datapoint, i);
+      int pos = get_nz_index(datapoint, i);
+      dist -= 2*val*cluster[pos];
+   }
+  if (fabs(dist) <1e-10)
+     dist = 0;
+  return sqrt(dist);
+}
+
+flt_dbl calc_chebychev_distance( sparse_flt_dbl_vec & datapoint,  sparse_flt_dbl_vec &cluster){
+   sparse_flt_dbl_vec diff = minus(datapoint , cluster);
+   flt_dbl ret = 0;
+   FOR_ITERATOR(i, diff){
+      ret = std::max(ret, (flt_dbl)fabs(get_nz_data(diff, i)));
+   }
+   return ret;
+
+}
+flt_dbl calc_chebychev_distance( sparse_flt_dbl_vec & datapoint,  flt_dbl_vec &cluster){
+   flt_dbl_vec diff = minus(datapoint , cluster);
+   flt_dbl ret = 0;
+   for (int i=0; i< diff.size(); i++)
+      ret = std::max(ret, (flt_dbl)fabs(diff[i]));
+
+   return ret;
+
+}
+
+flt_dbl calc_manhatten_distance( sparse_flt_dbl_vec & datapoint,  sparse_flt_dbl_vec &cluster){
+   sparse_flt_dbl_vec diff = minus(datapoint , cluster);
+   sparse_flt_dbl_vec absvec = fabs(diff);
+   flt_dbl ret = sum(absvec);
+   return ret;
+
+}
+flt_dbl calc_manhatten_distance( sparse_flt_dbl_vec & datapoint,  flt_dbl_vec &cluster){
+   flt_dbl_vec diff = minus(datapoint , cluster);
+   flt_dbl ret = sum(fabs(diff));
+   return ret;
+
+}
+
+flt_dbl calc_cosine_distance( sparse_flt_dbl_vec & datapoint,  sparse_flt_dbl_vec & cluster, flt_dbl sum_sqr, flt_dbl sum_sqr0){
+   flt_dbl dotprod = dot_prod(datapoint,cluster);
+   flt_dbl denominator = sqrt(sum_sqr0)*sqrt(sum_sqr);
+   return 1.0 - dotprod / denominator; 
+}
+
+flt_dbl calc_cosine_distance( sparse_flt_dbl_vec & datapoint,  flt_dbl_vec & cluster, flt_dbl sum_sqr, flt_dbl sum_sqr0){
+   flt_dbl dotprod = dot_prod(datapoint,cluster);
+   flt_dbl denominator = sqrt(sum_sqr0)*sqrt(sum_sqr);
+   return 1.0 - dotprod / denominator; 
+}
+
+
+flt_dbl calc_distance(sparse_flt_dbl_vec &datapoint,  sparse_flt_dbl_vec & cluster, flt_dbl sqr_sum, flt_dbl sqr_sum0){
+   switch(ac.distance_measure){
+      case EUCLIDEAN:          
+          return calc_euclidian_distance(datapoint, cluster, sqr_sum, sqr_sum0);
+      case CHEBYCHEV:
+          return calc_chebychev_distance(datapoint, cluster);
+      case COSINE:
+	  return calc_cosine_distance(datapoint, cluster, sqr_sum, sqr_sum0);  
+      case MANHATTAN:
+          return calc_manhatten_distance(datapoint, cluster);
+      case MANAHOLIS:
+      case WEIGHTED_MANAHOLIS:
+      case WEIGHTED:
+      default:
+          logstream(LOG_ERROR)<< "distance measure " << ac.distance_measure<< "  not implemented yet" << std::endl;
+    }
+    return -1;
+
+}
+
+
+flt_dbl calc_distance(sparse_flt_dbl_vec &datapoint,  flt_dbl_vec & cluster, flt_dbl sqr_sum, flt_dbl sqr_sum0){
+   switch(ac.distance_measure){
+      case EUCLIDEAN:          
+          return calc_euclidian_distance(datapoint, cluster, sqr_sum, sqr_sum0);
+      case CHEBYCHEV:
+          return calc_chebychev_distance(datapoint, cluster);
+      case COSINE:
+	  return calc_cosine_distance(datapoint, cluster, sqr_sum, sqr_sum0);  
+      case MANHATTAN:
+          return calc_manhatten_distance(datapoint, cluster);
+      case MANAHOLIS:
+      case WEIGHTED_MANAHOLIS:
+      case WEIGHTED:
+      default:
+          logstream(LOG_ERROR)<< "distance measure " << ac.distance_measure<< "  not implemented yet" << std::endl;
+    }
+    return -1;
+
+}
+
+/**
+ *
+
+v1 =
+         0    1.0000   -3.5000         0   0
+v2 =
+
+         1    2        3               4   5
+v3 =
+         0    0.5000   0               4   0
+
+
+ *
+ *
+ *
+ * */
+void test_distance(){
+  sparse_flt_dbl_vec v1;
+  set_size(v1, 5);
+  set_new(v1,1,1.0);
+  set_new(v1,2,-3.5);
+
+  sparse_flt_dbl_vec v3;
+  set_size(v3, 5);
+  set_new(v3,1,0.5);
+  set_new(v3,3,4);
+  flt_dbl_vec v2 = init_vec("1 2 3 4 5", 5);
+  ac.distance_measure = EUCLIDEAN;
+  flt_dbl ret = calc_distance(v1, v2, sum_sqr(v2), sum_sqr(v1));
+  assert(powf(ret - 9.233092656309694,2) < 1e-10);
+
+  ret = calc_distance(v1, v1, sum_sqr(v1), sum_sqr(v1));
+  assert(powf(ret - 0, 2) < 1e-10);
+
+  ret = calc_distance(v1, v3, sum_sqr(v1), sum_sqr(v3));
+  assert(powf(ret - 5.3385,2) <1e-8);
+
+  ac.distance_measure = COSINE;
+  ret = calc_distance(v1, v2, sum_sqr(v1), sum_sqr(v2));
+  assert(powf(ret - 1.3149, 2)<1e-8);
+
+  ret = calc_distance(v1, v3, sum_sqr(v1), sum_sqr(v3));
+  assert(powf(ret - (1 - .0341), 2)<1e-8);
+
+
+}
+
---
+++ demoapps/clustering/unittest.hpp
@@ -0,0 +1,62 @@
+#ifndef _UNITTEST_HPP
+#define _UNITTEST_HPP
+
+#include "clustering.h"
+#include "../gabp/advanced_config.h"
+
+extern advanced_config ac;
+
+void test_distance();
+/**
+ * UNIT TESTING
+ */
+void verify_result(double obj, double train_rmse, double validation_rmse){
+   assert(ac.unittest > 0);
+   switch(ac.unittest){
+      case 1: //ALS: Final result. Obj=0.0114447, TRAIN RMSE= 0.0033 VALIDATION RMSE= 1.1005.
+	 break;
+
+      case 3:
+         assert(fabs(ps.cost - 0.522652) < 1e-5);
+         break;
+   }
+}
+
+
+//UNIT TESTING OF VARIOUS METHODS
+void unit_testing(int unittest, graphlab::command_line_options& clopts){
+   if (unittest == 1){
+      test_math();
+      exit(0);
+   }
+   else if (unittest == 2){
+      test_distance();
+      exit(0);
+   }
+   /*if (unittest == 1){ //ALTERNATING LEAST SQUARES
+      //ac.datafile = "als"; ps.algorithm = ALS_MATRIX; ac.algorithm = ALS_MATRIX; ac.FLOAT = true; ac.als_lambda = 0.001;
+      //clopts.set_scheduler_type("round_robin(max_iterations=100,block_size=1)");
+   }*/
+   else if (unittest == 3){
+      ac.datafile = "cluster5x5";
+      ac.algorithm = K_MEANS_FUZZY;
+      ac.iter = 10;
+      ac.init_mode = 0;
+      ac.debug=true;
+      clopts.set_ncpus(1);
+      ac.K = 3;
+   
+   }
+   else if (unittest == 4){
+     test_fmath();
+     exit(0);
+   }
+   else {
+      logstream(LOG_ERROR) << " Unit test mode " << unittest << " is not supported yet! " << std::endl;
+      exit(1);
+   }
+}
+
+
+
+#endif
