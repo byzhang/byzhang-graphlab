7ad59591170b662b387335e70b1efcab64a4552c
Jim Lim <jiunnhal@cmu.edu>
2012-01-04 10:37:54 -0500

    Added setScope, setScheduler, and setNCpus

---
+++ extapis/java_jni/src/org/graphlab/Updater.java
@@ -0,0 +1,67 @@
+package org.graphlab;
+
+/**
+ * Updater
+ * 
+ * <p>The GraphLab engine will invoke an updater on each scheduled node. Extend
+ * this class to provide an update function for that node. Note that the update
+ * function may update node data, modify edge data, and schedule neighbors, but
+ * may not modify the graph structure. You may reuse the updater object on
+ * across multiple vertices (this is encouraged). Engine uses {@link #id()} to
+ * uniquely identify updaters.</p>
+ * 
+ * @author Jiunn Haur Lim <jiunnhal@cmu.edu>
+ */
+public abstract class Updater {
+
+	/** Flag to indicate that the updater has no ID */
+	public static final int ID_NOT_SET = -1;
+
+	/**
+	 * Identifier for this updater Will be set when this updater is first
+	 * scheduled.
+	 */
+	private int mId = ID_NOT_SET;
+
+	/**
+	 * Updates the vertex identified by <tt>vertex_id</tt>. Subclasses may wish
+	 * to maintain a reference to the graph object.
+	 * 
+	 * @param context      graphlab context; use {@link Context#schedule(int, Updater)}
+	 *                     to schedule vertices.
+	 * @param vertexId     application vertex ID
+	 */
+	public abstract void update(Context context, int vertexId);
+
+	/**
+	 * Do not call. This is only useful to the scheduler.
+	 * 
+	 * @return ID of this updater
+	 */
+	protected final int id() {
+		return mId;
+	}
+
+	/**
+	 * Do not call. This is only useful to the scheduler.
+	 * 
+	 * @param id
+	 *            ID of this updater
+	 */
+	protected final void setId(int id) {
+		mId = id;
+	}
+
+	/* 
+	 * Two updaters are equal if they share the same id.
+	 * (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals (Object other) {
+		if (null == other || !(other instanceof Updater))
+			return false;
+		return mId == ((Updater) other).id();
+	}
+
+}
---
+++ extapis/java_jni/src/org/graphlab/util/GraphLoader.java
@@ -0,0 +1,82 @@
+package org.graphlab.util;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+import org.graphlab.data.ScalarEdge;
+import org.graphlab.data.ScalarVertex;
+
+/**
+ * Utility functions for loading graphs from various file formats.
+ * 
+ * @author Jiunn Haur Lim <jiunnhal@cmu.edu>
+ */
+public class GraphLoader {
+
+	private static final Logger logger = Logger.getLogger(GraphLoader.class);
+	
+	/**
+	 * Load a graph file specified in the format:
+	 * 
+	 * <code>
+	 * source_id <tab> target_id
+	 * source_id <tab> target_id
+	 * source_id <tab> target_id
+	 * </code>
+	 * 
+	 * The file should not contain repeated edges, and should not contain 
+	 * self edges.
+	 * 
+	 * @param graph
+	 *            the graph to construct
+	 * @param filename
+	 *            the file to read from
+	 */
+	public static void loadGraphFromTsvFile(
+			MutableGraph<ScalarVertex, ScalarEdge> graph, String filename)
+			throws IOException {
+
+		
+		BufferedReader reader = new BufferedReader(new FileReader(filename));
+		
+		String input;
+		int lineNumber = 1;
+		
+		while (null != (input = reader.readLine())) {
+
+			String[] tokens = input.split(" ");
+			if (tokens.length != 3)
+				throw new IOException("Line " + lineNumber + " did not have exactly two tokens.");
+
+			int source = Integer.parseInt(tokens[0]);
+			int target = Integer.parseInt(tokens[1]);
+			float weight = Float.parseFloat(tokens[2]);
+
+			// ensure that the number of vertices is correct
+			if (source >= graph.size() || target >= graph.size())
+				resizeGraph (graph, Math.max(source, target) + 1);
+			if (source != target) {
+				graph.addEdge(new ScalarEdge(source, target, weight));
+			}else {
+				logger.warn("Dropped self-edge for vertex " + source);
+			}
+
+		}
+
+		logger.trace ("Finished loading graph with: " + graph.size() + " vertices.");
+
+	}
+	
+	private static void resizeGraph (MutableGraph<ScalarVertex, ScalarEdge> graph, int count){
+		
+		int difference = count - graph.size();
+		while (difference > 0){
+			graph.addVertex(new ScalarVertex(0));
+			difference--;
+		}
+		
+	}
+
+}
