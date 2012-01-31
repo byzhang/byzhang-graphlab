9e7f8c3ab22f2f7e55e7bd3d47972c8ea56e1a26
Danny Bickson <danny.bickson@gmail.com>
2012-01-31 02:44:16 -0500

    fixed graph3 to work correctly with incoming edges, first working version of binary svd graph3

---
+++ extapis/java_jni/src/org/graphlab/Core.java
@@ -0,0 +1,424 @@
+package org.graphlab;
+
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+import org.graphlab.data.Vertex;
+import org.jgrapht.DirectedGraph;
+
+/**
+ * GraphLab Core.
+ * 
+ * <p>
+ * This interfaces with the C++ library via
+ * <abbr title="Java Native Interface">JNI</abbr> and
+ * mirrors <tt>graphlab::core</tt>.
+ * </p>
+ * 
+ * <p>
+ * All logging from this core is done via {@link org.apache.log4j.Logger}.
+ * To configure, retrieve the logger using <tt>Logger.getClass(Core.class)</tt>.
+ * </p>
+ * 
+ * @author Jiunn Haur Lim <jiunnhal@cmu.edu>
+ * @see <a href="http://logging.apache.org/log4j/1.2/manual.html">Log4J</a>
+ */
+public final class Core {
+
+	/** Logger (Java layer only, the C++ layer has its own logger) */
+	private static final Logger logger = Logger.getLogger (Core.class);
+
+	/** Indicates if the core has been destroyed and cannot be reused. */
+	private boolean mDestroyed = false;
+
+	/** Address of graphlab::core object */
+	private long mCorePtr;
+
+	static {
+		// load the JNI library
+		System.loadLibrary("graphlabjni");
+		logger.trace ("JNI library libgraphlabjni loaded.");
+	}
+
+	/**
+	 * Creates a new GraphLab core.
+	 * <b>Call {@link #destroy()} when done to free up resources.</b>
+	 * 
+	 * @throws CoreException if there was an error creating the core
+	 */
+	public Core() throws CoreException {
+
+		mCorePtr = createCore();
+		if (0 >= mCorePtr)
+			throw new CoreException("Unable to create a core.");
+		logger.trace ("Core created.");
+
+	}
+	
+	 /**
+   * Creates a new GraphLab core.
+   * <b>Call {@link #destroy()} when done to free up resources.</b>
+   * 
+   * @param config
+   *          configuration e.g. scheduler, scope
+   * @throws CoreException
+   *          if there was an error creating the core
+   */
+  public Core(CoreConfiguration config) throws CoreException {
+
+    mCorePtr = createCore(config.toString());
+    if (0 >= mCorePtr)
+      throw new CoreException("Unable to create a core.");
+    logger.trace ("Core created.");
+
+  }
+
+  /**
+   * Tells core to operate on this graph. This creates a proxy graph in the
+   * GraphLab engine. Updates are forwarded to the Java updater (which you will
+   * provide through {@link #schedule(Vertex, Updater)}.)
+   * 
+   * <b> Once this is called, your graph must not be modified (or there be
+   * dragons!) </b>
+   * 
+   * @param graph
+   *          the graph for the core operate on.
+   * @param <G>
+   *          Graph type must implement {@link org.jgrapht.DirectedGraph}
+   * @param <V>
+   *          Vertex type must implement {@link org.graphlab.data.Vertex}.
+   * 
+   * @throws NullPointerException
+   *           if graph is null
+   * @throws IllegalArgumentException
+   *           if graph is empty
+   * @throws IllegalStateException
+   *           if {@link #destroy()} was already invoked on this object
+   */
+	public <G extends DirectedGraph<V, E>, V extends Vertex, E>
+	void setGraph(G graph) {
+
+		if (null == graph)
+			throw new NullPointerException("graph must not be null.");
+		
+		if (mDestroyed)
+			throw new IllegalStateException("Core has been destroyed and may not be reused.");
+		
+		// inspect vertices
+		Set<? extends Vertex> vertices = graph.vertexSet();
+		if (null == vertices || 0 == vertices.size())
+			throw new IllegalArgumentException("graph must not be empty.");
+		
+		long startTime = System.currentTimeMillis();
+
+		// add vertices
+		for (Vertex vertex : vertices)
+		  vertex.setRawId(addVertex(mCorePtr, vertex));
+
+		// add edges
+		for (E edge : graph.edgeSet())
+		  addEdge(mCorePtr, graph.getEdgeSource(edge).rawId(), graph.getEdgeTarget(edge).rawId());
+
+		long elapsed = System.currentTimeMillis() - startTime;
+		logger.info ("Graph transferring took: " + elapsed + " ms.");
+
+	}
+
+  /**
+   * Destroys the GraphLab core. Once destroyed, this object may not be used.
+   * Calling {@link #destroy()} more than once on the same object will generate
+   * a warning in the logs but will not any other effects.
+   */
+	public void destroy() {
+
+		if (mDestroyed) {
+			logger.warn("Core has already been destroyed and may not be destroyed again.");
+			return;
+		}
+
+		if (0 == mCorePtr)
+			throw new IllegalStateException("Core missing or was never allocated.");
+
+		destroyCore(mCorePtr);
+		mDestroyed = true;
+		logger.trace("Core destroyed.");
+
+	}
+
+  /**
+   * Run the engine until a termination condition is reached or there are no
+   * more tasks remaining to execute. <b>{@link #setGraph(DirectedGraph)} must
+   * be called before invoking this method</b>.
+   * 
+   * @return runtime
+   * @throws IllegalStateException
+   *           if {@link #destroy()} was invoked on this object
+   */
+	public double start() {
+		
+		if (mDestroyed)
+			throw new IllegalStateException("Core has been destroyed and may not be reused.");
+		
+		logger.info("GraphLab engine started.");
+		return start(mCorePtr);
+		
+	}
+	
+	/**
+	 * Get the number of updates executed by the engine.
+	 * @return update count
+   * @throws IllegalStateException
+   *           if {@link #destroy()} was invoked on this object
+	 */
+	public long lastUpdateCount(){
+	  
+	  if (mDestroyed)
+      throw new IllegalStateException("Core has been destroyed and may not be reused.");
+	  
+	  return lastUpdateCount(mCorePtr);
+	  
+	}
+
+  /**
+   * Schedule the execution of an update function on a particular vertex.
+   * 
+   * @param vertexId
+   *          application vertex ID
+   * @param updater
+   *          updater to execute
+   * @throws NullPointerException
+   *           if <tt>updater</tt> or <tt>vertex</tt> was null.
+   * @throws IllegalStateException
+   *           if {@link #destroy()} was invoked on this object
+   */
+	public void schedule(Vertex vertex, Updater<?> updater) {
+
+		if (null == updater || null == vertex)
+			throw new NullPointerException("updater and vertex must not be null.");
+		
+		if (mDestroyed)
+			throw new IllegalStateException("Core has been destroyed and may not be reused.");
+
+		schedule(mCorePtr, updater, vertex.rawId());
+		
+	}
+	
+	/**
+	 * Schedules the execution of the update function on all vertices.
+	 * 
+	 * @param updater
+	 *         updater to execute
+   * @throws NullPointerException
+   *           if <tt>updater</tt> was null
+   * @throws IllegalStateException
+   *           if {@link #destroy()} was invoked on this object
+	 */
+	public void scheduleAll(Updater<?> updater){
+	  
+	  if (null == updater)
+	    throw new NullPointerException("updater must not be null.");
+	  
+	  if (mDestroyed)
+	    throw new IllegalStateException("Core has been destroyed and may not be reused.");
+
+	  scheduleAll(mCorePtr, updater);
+	  
+	}
+	
+  /**
+   * Sets the number of CPUs that the engine will use. This will destroy the
+   * current engine and any tasks associated with the current scheduler.
+   * 
+   * @param ncpus
+   *          number of CPUs that the engine will use.
+   * 
+   * @throws IllegalArgumentException
+   *           if <tt>ncpus</tt> was negative.
+   * @throws IllegalStateException
+   *           if {@link #destroy()} has already been invoked on this object
+   */
+	public void setNCpus(long ncpus){
+	  if (0 >= ncpus)
+	    throw new IllegalArgumentException ("ncpus must be positive.");
+	  if (mDestroyed)
+      throw new IllegalStateException("Core has been destroyed and may not be reused.");
+	  setNCpus(mCorePtr, ncpus);
+	}
+	
+  /**
+   * Sets the type of scheduler. This only sets the type, and ignores any
+   * scheduler options. This will destroy the current engine and any tasks
+   * currently associated with the scheduler.
+   * 
+   * @param scheduler
+   * @throws NullPointerException
+   *           if <tt>scheduler</tt> was null
+   * @throws IllegalStateException
+   *           if {@link #destroy()} has already been invoked on this object
+   */
+  public void setSchedulerType(Scheduler scheduler) {
+    if (null == scheduler)
+      throw new NullPointerException("scheduler must not be null.");
+    if (mDestroyed)
+      throw new IllegalStateException("Core has been destroyed and may not be reused.");
+    setSchedulerType(mCorePtr, scheduler.type());
+  }
+	
+  /**
+   * Sets the scope consistency model that the engine will use. This will
+   * destroy the current engine and any tasks currently associated with the
+   * scheduler.
+   * 
+   * @param scope
+   * @throws NullPointerException
+   *           if <tt>scope</tt> was null
+   * @throws IllegalStateException
+   *           if {@link #destroy()} has already been invoked on this object
+   */
+	public void setScopeType(Scope scope){
+	  if (null == scope)
+	    throw new NullPointerException("scope must not be null.");
+	  if (mDestroyed)
+	    throw new IllegalStateException("Core has been destroyed and may not be reused.");
+	  setScopeType(mCorePtr, scope.toString());
+	}
+	
+  /**
+   * Creates and initializes graphlab::core &raquo; dynamically allocates a
+   * core. Must be freed by a corresponding call to {@link #destroyCore()}.
+   * 
+   * @return address of core or 0 on failure
+   */
+	private native long createCore();
+
+  /**
+   * Creates and initializes graphlab::core &raquo; dynamically allocates a
+   * core. Must be freed by a corresponding call to {@link #destroyCore()}.
+   * 
+   * @return address of core or 0 on failure
+   */
+	private native long createCore(String command_line_args);
+
+  /**
+	 * Deletes the graphlab::core that was allocated in {@link #initCore()}.
+	 * 
+	 * @param ptr
+	 *        {@link #mCorePtr}
+	 */
+	private native void destroyCore(long ptr);
+	
+	/**
+	 * Add additional vertices up to <tt>n</tt>. This will fail if resizing down. 
+	 * @param ptr
+	 * 			{@link #mCorePtr}
+	 * @param n
+	 * 			number of vertices to add
+	 */
+	private native void resizeGraph(long ptr, int n);
+	
+	/**
+	 * Adds a vertex to the native graph.
+	 * @param ptr
+	 * 			{@link #mCorePtr}
+	 * @param vertex
+	 * 			application vertex ID
+	 * @return
+	 * 			graphlab vertex ID
+	 */
+	private native int addVertex(long ptr, Vertex vertex);
+
+	/**
+	 * Adds an edge to the native graph.
+	 * @param ptr
+	 * 			{@link #mCorePtr}
+	 * @param source
+	 * 			graphlab vertex ID
+	 * @param target
+	 * 			graphlab vertex ID
+	 */
+	private native void addEdge(long ptr, int source, int target);
+	
+	/**
+	 * Add a single update function to a single vertex
+	 * @param core_ptr
+	 *       {@link #mCorePtr}
+	 * @param updater
+	 * @param vertexId
+	 *       graphlab vertex ID
+	 */
+	private native void schedule(long core_ptr, Updater<?> updater, int vertexId);
+
+	/**
+	 * Add the given function to all vertices using the given priority
+	 * @param core_ptr
+	 *       {@link #mCorePtr}
+   * @param updater
+	 */
+	private native void scheduleAll(long core_ptr, Updater<?> updater);
+	
+  /**
+   * Run the engine until a termination condition is reached or there are no
+   * more tasks remaining to execute.
+   * @param ptr
+   *      {@link #mCorePtr}
+   * @return runtime
+   */
+	private native double start(long ptr);
+	
+	/**
+	 * Gets the number of updates executed by the engine.
+	 * @param ptr {@link Core#mCorePtr}
+	 * @return update count
+	 */
+	private native long lastUpdateCount(long ptr);
+
+	/**
+	 * Set the number of cpus that the engine will use.
+	 * This will destroy the current engine and any tasks associated with the current scheduler.
+	 * If this is not what you want, then configure the core in the constructor instead.
+	 * 
+	 * @param ptr
+	 *       {@link #mCorePtr}
+	 * @param ncpus
+	 *       number of CPUs
+	 */
+	private native void setNCpus(long ptr, long ncpus);
+	
+	/**
+	 * Set the type of scheduler. 
+	 * This will destroy the current engine and any tasks currently associated with the scheduler.
+	 * If this is not what you want, then configure the core in the constructor instead.
+	 * 
+	 * @param ptr
+	 *       {@link #mCorePtr}
+	 * @param schedulerType
+	 */
+	private native void setSchedulerType(long ptr, String schedulerType);
+	
+	/**
+	 * Set the scope consistency model used in this engine. 
+	 * This will destroy the current engine and any tasks associated with the current scheduler.
+	 * If this is not what you want, then configure the core in the constructor instead.
+	 * 
+	 * @param ptr
+	 *       {@link #mCorePtr}
+	 * @param scopeType
+	 */
+	private native void setScopeType(long ptr, String scopeType);
+	
+	/**
+	 * Generic exception for dealing with GraphLab cores
+	 * 
+	 * @author Jiunn Haur Lim
+	 */
+	public static class CoreException extends Exception {
+
+		private static final long serialVersionUID = -1231034883125084972L;
+
+		public CoreException(String string) {
+			super(string);
+		}
+
+	}
+
+}
---
+++ extapis/java_jni/src/org/graphlab/demo/PageRank.java
@@ -0,0 +1,232 @@
+package org.graphlab.demo;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.graphlab.Context;
+import org.graphlab.Core;
+import org.graphlab.Core.CoreException;
+import org.graphlab.Updater;
+import org.graphlab.data.ScalarVertex;
+import org.graphlab.util.GraphLoader;
+import org.jgrapht.DirectedGraph;
+import org.jgrapht.graph.DefaultDirectedWeightedGraph;
+import org.jgrapht.graph.DefaultWeightedEdge;
+
+/**
+ * PageRank Algorithm
+ * 
+ * <p>Demonstrates GraphLab over JNI.</p>
+ * 
+ * @author Jiunn Haur Lim <jiunnhal@cmu.edu>
+ */
+public class PageRank {
+
+  private static final Logger logger = Logger.getLogger(PageRank.class);
+  
+  public static void main (String[] args){
+    
+    initLogger();
+    
+    // check arguments
+    if (!checkParams(args)) {
+      logger.trace("Exiting main method.");
+      return;
+    }
+
+    String filename = args[0];
+    logger.info("Graph file: " + filename);
+
+    // initialize graphlab core
+    final Core core;
+    try {
+      logger.trace("Initializing GraphLab core ...");
+      core = new Core();
+    } catch (CoreException e) {
+      logger.fatal("Unable to initialize core. Terminating.", e);
+      logger.trace("Exiting main method.");
+      return;
+    }
+
+    // construct graph
+    logger.trace("Constructing graph from " + filename + " ...");
+    final DefaultDirectedWeightedGraph<ScalarVertex, DefaultWeightedEdge> graph;
+    try {
+      graph = constructGraph(filename);
+    } catch (IOException e) {
+      logger.fatal("Unable to construct graph. Terminating.", e);
+      logger.trace("Exiting main method.");
+      core.destroy();
+      return;
+    }
+    
+    // execute graph updates
+    core.setGraph(graph);
+    core.scheduleAll(new PageRankUpdater(graph, 1));
+    logger.trace("Running graphlab ...");
+    logger.info("Took " + core.start() + " seconds.");
+    
+    // print results
+    printResults (graph);
+    logger.info("Update count: " + core.lastUpdateCount());
+    
+    // done
+    core.destroy();
+    
+    return;
+
+  }
+  
+  /** Initialize logger and set logging levels */
+  private static void initLogger(){
+    BasicConfigurator.configure();
+    Logger.getLogger(Core.class).setLevel(Level.INFO);
+    logger.setLevel(Level.INFO);
+  }
+  
+  /**
+   * Checks that required input parameters are available and valid. Prints
+   * instructions if not all parameters were valid.
+   * 
+   * @param args
+   *          array of program arguments
+   * @return true if parameters are OK; false otherwise
+   */
+  private static boolean checkParams(String[] args) {
+
+    if (args.length != 1) {
+      System.out.println("Please provide filename.");
+      System.out.println("Usage: java -Djava.library.path=... "
+          + ShortestPath.class.getCanonicalName() + " path/to/tsv/file");
+      return false;
+    }
+
+    return true;
+
+  }
+
+  private static DefaultDirectedWeightedGraph<ScalarVertex, DefaultWeightedEdge>
+    constructGraph(String filename)
+    throws IOException {
+
+    DefaultDirectedWeightedGraph<ScalarVertex, DefaultWeightedEdge> graph
+      = new DefaultDirectedWeightedGraph<ScalarVertex, DefaultWeightedEdge>(DefaultWeightedEdge.class);
+    GraphLoader.loadGraphFromTsvFile(graph, filename);
+    normalize(graph); 
+    
+    return graph;
+
+  }
+  
+  private static void normalize(DefaultDirectedWeightedGraph<ScalarVertex, DefaultWeightedEdge> graph){
+    
+    for(ScalarVertex vertex : graph.vertexSet()) {
+      double sum = 0;
+      Collection<DefaultWeightedEdge> outEdges = graph.outgoingEdgesOf(vertex);
+      // Sum up weight on out edges
+      for(DefaultWeightedEdge edge : outEdges){
+        sum += graph.getEdgeWeight(edge);
+      }
+      for(DefaultWeightedEdge edge : outEdges){
+        graph.setEdgeWeight(edge, (graph.getEdgeWeight(edge)/sum));
+      }
+    }
+    
+  }
+  
+  private static void printResults (DirectedGraph<ScalarVertex, DefaultWeightedEdge> g){
+      
+    logger.info("----------------- Results -----------------");
+    logger.info("ID : Rank");
+    
+    Collection<ScalarVertex> vertices = g.vertexSet();
+    List<ScalarVertex> verticesList = new ArrayList<ScalarVertex>(vertices.size());
+    for (ScalarVertex vertex : vertices){
+      verticesList.add(vertex);
+    }
+    
+    Collections.sort(verticesList, new Comparator<ScalarVertex>(){
+      public int compare(ScalarVertex left, ScalarVertex right) {
+        return Double.compare(left.value(), right.value());
+      }
+    });
+    
+    for (int i=0; i<Math.min(verticesList.size(), 5); i++){
+      ScalarVertex vertex = verticesList.get(i);
+      logger.info(vertex.id() + " : " + vertex.value());
+    }
+    
+  }
+
+  private static class PageRankUpdater extends Updater<ScalarVertex> {
+
+    /** Global reset probability */
+    public static final double RESET_PROB = 0.15;
+    
+    /** Global accuracy tolerance */
+    public static final double ACCURACY = 1e-5;
+    
+    /** Priority */
+    private double mPriority;
+    
+    private DefaultDirectedWeightedGraph<ScalarVertex, DefaultWeightedEdge> mGraph;
+
+    public PageRankUpdater(
+        DefaultDirectedWeightedGraph<ScalarVertex, DefaultWeightedEdge> graph,
+        double priority) {
+      if (null == graph) throw new NullPointerException ("graph must not be null.");
+      mGraph = graph;
+      mPriority = priority;
+    }
+
+    @Override
+    public void update(Context context, ScalarVertex vertex) {
+      
+      // Compute weighted sum of neighbors
+      double sum = 0;
+      
+      /* Iterate over edge_id_list and get source is slow in graph2 */
+      for(DefaultWeightedEdge edge : mGraph.incomingEdgesOf(vertex)){
+        double weight = mGraph.getEdgeWeight(edge);
+        sum += weight* 
+               mGraph.getEdgeSource(edge).value();
+      }
+      
+      // Add random reset probability
+      double oldValue = vertex.value();
+      vertex.setValue(RESET_PROB + (1-RESET_PROB)*sum);
+      for(DefaultWeightedEdge edge : mGraph.outgoingEdgesOf(vertex)) {    
+        double weight = mGraph.getEdgeWeight(edge);
+        double residual = weight * Math.abs(vertex.value()- oldValue);
+        // If the neighbor changed sufficiently add to scheduler.
+        if(residual > ACCURACY) 
+          context.schedule(
+              mGraph.getEdgeTarget(edge),
+              new PageRankUpdater(mGraph, residual)
+          );
+      }
+
+    }
+    
+    @Override
+    public double priority(){
+      return mPriority;
+    }
+    
+    @Override
+    public void add(Updater<ScalarVertex> other){
+      if (!(other instanceof PageRankUpdater))
+        throw new IllegalStateException("incompatible updaters added.");
+      mPriority += other.priority();
+    }
+
+  }
+  
+}
